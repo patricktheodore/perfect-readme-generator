c0a5675124f985777d18505e2b7d5c7b4e65a940
patricktheodore <patricktheodoresara@gmail.com>
2021-10-19 17:41:00 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+
+
+
+
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+
+
+
+
+
+//to do
+
+// application will be involed by using the following command node index.js
+
+//command-line application, 
+//inquirer - prompts for information about application repo
+//high quality readme is generated
+//inlcudes - title, description, table of contents, installation, usage, license, contributing, tests and questions. 
+//project title = title of readme
+//description, installation instructions, usage information, contribution, guidelines and test instructions = all have own sections
+//a choice of licenses is available in a list
+//a badge for that license is added near the top of the readme. a notice is added the license sections that explains which license thte applications is covered under
+//github username, added to questions section, with  a link to github profile
+//email address, added to questions sections with instructions on how to reach me with additional questions
+//working table of contents. 
