23299218545d828df8bece7a843259b3227aecd9
patricktheodore <patricktheodoresara@gmail.com>
2021-10-20 18:52:41 +0800

    mvp

--- index.js
+++ index.js
@@ -1,22 +1,125 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
-
-
-
+const fileName = 'README.md'
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your github username?',
+        name: 'gitUser',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is the title of your application/repository?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please provide a link to the application/repository.',
+        name: 'remoteRepo',
+    },
+    {
+        type: 'input',
+        message: 'Please provide a brief summary of your application/repository (max 1 or 2 lines).',
+        name: 'briefDescription',
+    },
+    {
+        type: 'input',
+        message: 'Please provide a more in depth description of the project.',
+        name: 'detailedDescription',
+    },
+    {
+        type: 'input',
+        message: 'What technologies/languages did you use to build your project?',
+        name: 'builtWith',
+    },
+    {
+        type: 'input',
+        message: 'Please provide any prerequisites for the application.',
+        name: 'prerequisites',
+    },
+    {
+        type: 'input',
+        message: 'Please provide detailed installation instructions.',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Please provide detailed usage instructions.',
+        name: 'usage',
+    },
+    {
+        type: 'list',
+        message: 'Please select the license that applies to your application/repository',
+        choices: [
+            'Apache license 2.0',
+            'Do What The F*ck You Want To Public License',
+            'GNU General Public License v3.0',
+            'BSD 3-clause License',
+            'MIT',
+            'Mozilla Public License 2.0'],
+        name: 'license',
+        default: 'MIT',
+    },
+    {
+        type: 'input',
+        message: 'Please provide details on how other devs can contribute.',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'Please provied details on any tests for your application.',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'Please provied details on how best to contact you.',
+        name: 'contact',
+    },
+    {
+        type: 'input',
+        message: 'If you wish to provide any acknowledgments, please do so here.',
+        name: 'acknowledgments',
+    },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function generateReadme() {
+    inquirer.prompt(questions)
+        .then((data) => {
+            // console.log(data)
+            writeToFile(fileName, data);
+            return data
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                console.log('error!')
+            } else {
+                console.log('success!')
+            }
+        });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), (err) =>
+        err ? console.log(err) : console.log('Success!')
+    );
+}
+
+function init() {
+    generateReadme();
+}
+
 init();
 
 
@@ -26,12 +129,16 @@ init();
 
 //to do
 
-// application will be involed by using the following command node index.js
+//file name will be README.md
+
+// application will be involed by using the following command - node index.js
 
 //command-line application, 
 //inquirer - prompts for information about application repo
 //high quality readme is generated
+
 //inlcudes - title, description, table of contents, installation, usage, license, contributing, tests and questions. 
+
 //project title = title of readme
 //description, installation instructions, usage information, contribution, guidelines and test instructions = all have own sections
 //a choice of licenses is available in a list
@@ -39,3 +146,5 @@ init();
 //github username, added to questions section, with  a link to github profile
 //email address, added to questions sections with instructions on how to reach me with additional questions
 //working table of contents. 
+
+//licenses https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/licensing-a-repository#disclaimer
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,168 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+let badge = '';
+let link = '';
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'Apache license 2.0':
+      badge = 'https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0';
+      break;
+    case 'Do What The F*ck You Want To Public License':
+      badge = 'https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/';
+      break;
+    case 'GNU General Public License v3.0':
+      badge = 'https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0';
+      break;
+    case 'BSD 3-clause License':
+      badge = 'https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause';
+      break;
+    case 'MIT':
+      badge = 'https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT';
+      break;
+    case 'Mozilla Public License 2.0':
+      badge = 'https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0';
+      break;
+  }
+  return badge
+}
+
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'Apache license 2.0':
+      link = 'https://opensource.org/licenses/Apache-2.0';
+      break;
+    case 'Do What The F*ck You Want To Public License':
+      link = 'http://www.wtfpl.net/about/';
+      break;
+    case 'GNU General Public License v3.0':
+      link = 'https://www.gnu.org/licenses/gpl-3.0';
+      break;
+    case 'BSD 3-clause License':
+      link = 'https://opensource.org/licenses/BSD-3-Clause';
+      break;
+    case 'MIT':
+      link = 'https://opensource.org/licenses/MIT';
+      break;
+    case 'Mozilla Public License 2.0':
+      link = 'https://opensource.org/licenses/MPL-2.0';
+      break;
+  }
+  return link
+ }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return ` 
+  [![License: ${data.license}](${renderLicenseBadge(data.license)})]  
+  
+  <h3 align="center">${data.title}</h3>
+  
+  <div>
+    <p align="center">
+      ${data.briefDescription}
+      <br />
+      <a href="${data.remoteRepo}"><strong>Explore the docs »</strong></a>
+      <br />
+      <br />
+      <a href="${data.remoteRepo}">View Demo</a>
+      ·
+      <a href="${data.remoteRepo}/issues">Report Bug</a>
+      ·
+      <a href="${data.remoteRepo}/issues">Request Feature</a>
+    </p>
+  </div>
+  
+  
+  
+  <!-- TABLE OF CONTENTS -->
+  <details>
+    <summary>Table of Contents</summary>
+    <ol>
+      <li>
+        <a href="#about-the-project">About The Project</a>
+        <ul>
+          <li><a href="#built-with">Built With</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#getting-started">Getting Started</a>
+        <ul>
+          <li><a href="#prerequisites">Prerequisites</a></li>
+          <li><a href="#installation">Installation</a></li>
+        </ul>
+      </li>
+      <li><a href="#usage">Usage</a></li>
+      <li><a href="#contributing">Contributing</a></li>
+      <li><a href="#license">License</a></li>
+      <li><a href="#contact">Contact</a></li>
+      <li><a href="#acknowledgments">Acknowledgments</a></li>
+    </ol>
+  </details>
+  
+  
+  <!-- ABOUT THE PROJECT -->
+  ## About The Project
+  ${data.detailedDescription}
+  <p align="right">(<a href="#top">back to top</a>)</p>
+  
+  ### Built With
+  ${data.builtWith}
+  <p align="right">(<a href="#top">back to top</a>)</p>
+  
+  
+  <!-- GETTING STARTED -->
+  ## Getting Started
+  <br />
+  
+  ### Prerequisites
+  ${data.prerequisites}
+  
+  ### Installation
+  ${data.installation}
+  <p align="right">(<a href="#top">back to top</a>)</p>
+  
+  
+  <!-- USAGE EXAMPLES -->
+  ## Usage
+  ${data.usage}
+  <p align="right">(<a href="#top">back to top</a>)</p>
+  
+  
+  <!-- CONTRIBUTING -->
+  ## Contributing
+  ${data.contribution}
+  <p align="right">(<a href="#top">back to top</a>)</p>
+  
+  
+  <!-- LICENSE -->
+  ## License
+  Distributed under the ${data.license}. See LICENSE.txt for more information.
+  
+  <p align="right">(<a href="#top">back to top</a>)</p>
+  
+  
+  <!-- TEST -->
+  ## Tests
+  ${data.tests}
+  <p align="right">(<a href="#top">back to top</a>)</p>
+  
+  
+  <!-- QUESTIONS & CONTACT -->
+  ## Questions & Contact Info
+  ${data.gitUser}
+  ${data.remoteRepo}
+  ${data.email}
+  ${data.contact}
+  <p align="right">(<a href="#top">back to top</a>)</p>
+  
+  
+  <!-- ACKNOWLEDGMENTS -->
+  ## Acknowledgments
+  ${data.acknowledgments}
+  <p align="right">(<a href="#top">back to top</a>)</p>  
 `;
 }
 
